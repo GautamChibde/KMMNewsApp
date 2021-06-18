0652b1f2a730e00e0923a125134087c1814dc333
Gautam <gautam.chibde@michelin.com>
2021-06-18 15:02:17 +0530

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMM_News_App"
+include(":News_App")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/newsapp/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.newsapp
+
+expect class Platform() {
+    val platform: String
+}
